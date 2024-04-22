b772590daf852fa3eba28b6013d8736c16a84a12
Shiham123 <s36651786@gmail.com>
2024-04-22 09:24:21 +0600

    responsive issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      xs: { min: '375px', max: '639px' },
+      xs: { max: '639px' },
       sm: { min: '640px', max: '767px' },
       md: { min: '768px', max: '1023px' },
       lg: { min: '1024px', max: '1279px' },
